4b492611305e4836a3c31d420b4a9113acb45df8
rennochj <rennochj@outlook.com>
2022-06-17 17:12:41 -0500

    update ec2-spot

--- modules/deven-ec2-spot/main.tf
+++ modules/deven-ec2-spot/main.tf
@@ -41,13 +41,13 @@ resource "aws_security_group" "deven_sg" {
   description = "deven security group"
   vpc_id      = var.aws_vpc
 
-  // To Allow SSH Transport
-  ingress {
-    from_port   = 22
-    protocol    = "tcp"
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # // To Allow SSH Transport
+  # ingress {
+  #   from_port   = 22
+  #   protocol    = "tcp"
+  #   to_port     = 22
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   egress {
     from_port   = 0
@@ -65,15 +65,30 @@ locals {
   initial_script = fileexists(var.initial_script) ? file(var.initial_script) : ""
 }
 
+resource "aws_kms_key" "deven_kms_key" {
+
+  description             = "KMS key for ebse encryption"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+
+}
+
 resource "aws_spot_instance_request" "deven_spot" {
 
   ami           = data.aws_ami.deven_ami.id
   instance_type = var.deven_instance_type
   # spot_price           = "0.1"
-  instance_interruption_behavior = "terminate"
-  spot_type                      = "persistent"
+  instance_interruption_behavior = "stop"
+  spot_type                      = "one-time"
   wait_for_fulfillment           = true
   key_name                       = aws_key_pair.deven_key.key_name
+  associate_public_ip_address    = true
+  iam_instance_profile           = "AmazonSSMRoleForInstancesQuickSetup"
+
+  root_block_device {
+    encrypted = true
+    kms_key_id = aws_kms_key.deven_kms_key.arn
+  }
 
   tags = {
     Name = var.deven_instance_name
@@ -85,6 +100,7 @@ resource "aws_spot_instance_request" "deven_spot" {
   user_data = <<EOF
     #!/bin/bash
     mkdir /workspace
+    [[ "/dev/xvdb: data" == $(file -s /dev/xvdb) ]] && mkfs -t xfs /dev/xvdb
     mount /dev/sdb /workspace
     echo '/dev/sdb /workspace xfs defaults,nofail 0 2' >> /etc/fstab
     chown ec2-user /workspace
@@ -114,7 +130,7 @@ resource "aws_cloudwatch_metric_alarm" "deven_cpu_alarm" {
     InstanceId = aws_spot_instance_request.deven_spot.spot_instance_id
   }
   alarm_actions = [
-    "arn:aws:automate:${var.aws_region}:ec2:terminate"
+    "arn:aws:automate:${var.aws_region}:ec2:stop"
   ]
 
 }
